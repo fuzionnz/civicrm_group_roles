c859fb9151180d4895192d5f3f35d668724a9384
Matt Trim <matt@circle-interactive.co.uk>
2021-06-10 13:50:47 +0100

    The entity.manager service in D8 uses entity_type.manager under the hood anyway

--- src/CivicrmGroupRoleRuleListBuilder.php
+++ src/CivicrmGroupRoleRuleListBuilder.php
@@ -52,7 +52,7 @@ class CivicrmGroupRoleRuleListBuilder extends ConfigEntityListBuilder {
   public static function createInstance(ContainerInterface $container, EntityTypeInterface $entity_type) {
     return new static(
       $entity_type,
-      $container->get('entity.manager')->getStorage($entity_type->id()),
+      $container->get('entity_type.manager')->getStorage($entity_type->id()),
       $container->get('entity_type.manager')->getStorage('user_role'),
       $container->get('civicrm_group_roles')
     );
